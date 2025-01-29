8bf34043919cd53e05ca11a3fd10ae8e814a60b3
Andrei Mocanu <andreimcornel@gmail.com>
2025-01-29 13:09:02 +0200

    Sections layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { Button } from '@/components/ui/button'
+import { Box } from '@chakra-ui/react'
 
 const App = () => {
   return (
-    <div>
-      <Button>Padel</Button>
-    </div>
+    <>
+      {/* Section 1 */}
+      <Box as='section' h='100vh' bg='black' />
+
+      {/* Section 2 */}
+      <Box as='section' h='100vh' bg='white' />
+
+      {/* Section 3 */}
+      <Box as='section' h='100vh' bg='green' />
+
+      {/* Section 4 */}
+      <Box as='section' h='100vh' bg='purple' />
+
+      {/* Section 5 */}
+      <Box as='section' h='100vh' bg='blue' />
+    </>
   )
 }
 
